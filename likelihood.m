6ea87b3081d4705b38a171eadc3a83f45fbe01d6
Yiming Hu <yiming.hu@ligo.org>
2013-02-05 17:53:21 +0000

    correct Metropolis ratio! at least for the first toy model, it bahaves exactly as theoretical prediction

--- likelihood.m
+++ likelihood.m
@@ -8,18 +8,10 @@ function chi2 = likelihood (params,t,f_of_t,sigma)
 
 %y = params(1)*sin(params(2)*t);%+params(3))%+params(4)*sin(params(5)*t+params(6));
 
-%y = params(1)*sin(params(2)+t);
+y = params(1)*sin(params(2)+t);
 
 % the generation of data are actually a combination of two sin function
 % but using only one sin funciton to fit
 % purpose: make the likelihood function have different peaks with different likelihood.
-%chi2 = sum(((f_of_t - y)/sigma).^2);
-
-
-% now I'm constructing a manuelly setted likelihood function.
-% in this 2 dimensional function, I will set the 'likelihood' to be
-% abs(sinc(x_1))*abs(sinc(x_2)). In this case, the peak will have different prior and volume.
-% notice that sinc function can be negative, while the likelihood should be always non-negative.
-
-chi2 = -2*log(abs(sinc(params(1)))*abs(sinc(params(2))));
+chi2 = sum(((f_of_t - y)/sigma).^2);
 return 
